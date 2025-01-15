60841f144a3b5956651344a3a25b688f20d6ee91
anekil <235892@edu.p.lodz.pl>
2025-01-15 15:09:05 +0100

    gan - passive_concrete

--- inpainting/InpaintingDataset.py
+++ inpainting/InpaintingDataset.py
@@ -5,30 +5,49 @@ import numpy as np
 import torch
 import random
 
-from PIL import Image, ImageDraw
+from PIL import Image
+from torchvision.transforms import v2
 
 
 class InpaintingDataset(torch.utils.data.Dataset):
     def __init__(self, df, transform=None):
         self.df = df
         self.transform = transform
+        self.to_tensor = v2.Compose([v2.ToImage(), v2.ToDtype(torch.float32, scale=True)])
 
     def apply_damage(self, image):
         img = np.array(image)
         height, width, _ = img.shape
-        mask = np.ones((height, width), dtype=np.uint8) * 255  # Initialize white mask
+        mask = np.ones((height, width), dtype=np.uint8) * 255
 
-        num_blobs = random.randint(3, 6)  # Number of blobs
-        max_radius = min(height, width) // 8  # Maximum radius for blobs
+        num_shapes = random.randint(4, 8)
+        max_size = min(height, width) // 8
 
-        for _ in range(num_blobs):
-            radius = random.randint(max_radius // 4, max_radius)
-            center_x = random.randint(radius, width - radius)
-            center_y = random.randint(radius, height - radius)
+        for _ in range(num_shapes):
+            shape_type = random.choice(["circle", "ellipse", "rectangle"])
+            if shape_type == "circle":
+                radius = random.randint(max_size // 4, max_size)
+                center_x = random.randint(radius, width - radius)
+                center_y = random.randint(radius, height - radius)
+                cv2.circle(mask, (center_x, center_y), radius, 0, -1)
+                cv2.circle(img, (center_x, center_y), radius, (255, 255, 255), -1)
 
-            # Draw a filled circle on the mask (black) and image (white damage)
-            cv2.circle(mask, (center_x, center_y), radius, 0, -1)  # Black blob on mask
-            cv2.circle(img, (center_x, center_y), radius, (255, 255, 255), -1)  # White blob on image
+            elif shape_type == "ellipse":
+                center_x = random.randint(max_size, width - max_size)
+                center_y = random.randint(max_size, height - max_size)
+                axis_x = random.randint(max_size // 4, max_size)
+                axis_y = random.randint(max_size // 4, max_size)
+                angle = random.randint(0, 360)
+                cv2.ellipse(mask, (center_x, center_y), (axis_x, axis_y), angle, 0, 360, 0, -1)
+                cv2.ellipse(img, (center_x, center_y), (axis_x, axis_y), angle, 0, 360, (255, 255, 255), -1)
+
+            elif shape_type == "rectangle":
+                top_left_x = random.randint(0, width - max_size)
+                top_left_y = random.randint(0, height - max_size)
+                bottom_right_x = top_left_x + random.randint(max_size // 4, max_size)
+                bottom_right_y = top_left_y + random.randint(max_size // 4, max_size)
+                cv2.rectangle(mask, (top_left_x, top_left_y), (bottom_right_x, bottom_right_y), 0, -1)
+                cv2.rectangle(img, (top_left_x, top_left_y), (bottom_right_x, bottom_right_y), (255, 255, 255), -1)
 
         return Image.fromarray(img), Image.fromarray(mask)
 
@@ -46,15 +65,14 @@ class InpaintingDataset(torch.utils.data.Dataset):
         if image.mode != 'RGB':
             image = image.convert('RGB')
 
-        damaged_image, mask = self.apply_damage(image)
-
         if self.transform:
-            original_image = self.transform(image)
-            damaged_image = self.transform(damaged_image)
-            mask = self.transform(mask).squeeze(0)
-            damaged_image = torch.cat([damaged_image, mask.unsqueeze(0)], dim=0)
-        else:
-            original_image = image
+            image = self.transform(image)
+
+        damaged_image, mask = self.apply_damage(image)
+        original_image = self.to_tensor(image)
+        damaged_image = self.to_tensor(damaged_image)
+        mask = self.to_tensor(mask)
+        damaged_image = torch.cat([damaged_image, mask], dim=0)
 
         return damaged_image, original_image
 

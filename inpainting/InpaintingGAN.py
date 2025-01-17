4e11bf927927d5c13fed1325e6d1035ed24e2bbb
anekil <235892@edu.p.lodz.pl>
2025-01-17 20:13:27 +0100

    gan - selected_land

--- inpainting/InpaintingGAN.py
+++ inpainting/InpaintingGAN.py
@@ -2,7 +2,6 @@ import torch
 import torch.nn as nn
 import pytorch_lightning as pl
 import torch.nn.functional as F
-import torchvision.models as models
 import torchvision
 import numpy as np
 
@@ -34,7 +33,6 @@ class Generator(nn.Module):
             nn.BatchNorm2d(128),
             nn.ReLU()
         )
-        self.attention = Attention(128)
         self.decoder = nn.Sequential(
             nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1),
             nn.ReLU(),
@@ -44,53 +42,122 @@ class Generator(nn.Module):
 
     def forward(self, x):
         x = self.encoder(x)
-        x = self.attention(x)
         x = self.decoder(x)
         return x
 
-
-class Critic(nn.Module):
+class PatchCritic(nn.Module):
     def __init__(self):
         super().__init__()
         self.model = nn.Sequential(
             nn.Conv2d(3, 64, kernel_size=4, stride=2, padding=1),
-            nn.LeakyReLU(0.2),
+            nn.LeakyReLU(0.1),
             nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),
             nn.BatchNorm2d(128),
-            nn.LeakyReLU(0.2),
-            nn.Conv2d(128, 1, kernel_size=4, stride=1, padding=0)
+            nn.LeakyReLU(0.1),
+            nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1),  # Increased number of channels
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.1),
+            nn.Conv2d(256, 1, kernel_size=4, stride=1, padding=0)
         )
 
     def forward(self, x):
-        return self.model(x).view(-1)
+        return self.model(x)
+
+
+class SharedFeaturesExtractor(nn.Module):
+    def __init__(self):
+        super().__init__()
+        model = torchvision.models.vgg11(weights='IMAGENET1K_V1').features
+        self.layers = nn.ModuleList([
+            model[0:2],
+            model[2:4],
+            model[4:7],
+        ])
+        for param in self.parameters():
+            param.requires_grad = False
+
+    def forward(self, x, layer_ids):
+        features = []
+        for i, layer in enumerate(self.layers):
+            x = layer(x)
+            if i in layer_ids:
+                features.append(x)
+        return features
+
+
+class PerceptualLoss(nn.Module):
+    def __init__(self, feature_extractor):
+        super().__init__()
+        self.feature_extractor = feature_extractor
+        self.layer_ids = [0, 1, 2]
+
+    def forward(self, pred, target):
+        pred_features = self.feature_extractor(pred, self.layer_ids)
+        target_features = self.feature_extractor(target, self.layer_ids)
+        loss = sum(F.l1_loss(p, t) for p, t in zip(pred_features, target_features))
+        return loss
+
+
+
+class TextureLoss(nn.Module):
+    def __init__(self, feature_extractor):
+        super().__init__()
+        self.feature_extractor = feature_extractor
+        self.layer_ids = [0, 1]
+
+    @staticmethod
+    def gram_matrix(features):
+        b, c, h, w = features.size()
+        features = features.view(b, c, h * w)
+        gram = torch.bmm(features, features.permute(0, 2, 1))
+        return gram / (c * h * w)
+
+    def forward(self, pred, target):
+        pred_features = self.feature_extractor(pred, self.layer_ids)
+        target_features = self.feature_extractor(target, self.layer_ids)
+        loss = sum(F.l1_loss(self.gram_matrix(p), self.gram_matrix(t))
+                   for p, t in zip(pred_features, target_features))
+        return loss
+
+class DiversityLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, generated_images):
+        batch_size = generated_images.size(0)
+        diversity_loss = 0
+        for i in range(batch_size):
+            for j in range(i + 1, batch_size):
+                diversity_loss += F.mse_loss(generated_images[i], generated_images[j])
+        return -diversity_loss / (batch_size * (batch_size - 1))
 
 class GANInpainting(pl.LightningModule):
-    def __init__(self, lr=1e-4):
+    def __init__(self, lr=1e-4, lambda_gp=20,
+                 g_adv_weight=1.0, g_rec_weight=2.0, g_perc_weight=3.0, g_tex_weight=2.0, g_div_weight=3.0,):
         super(GANInpainting, self).__init__()
         self.save_hyperparameters()
 
         self.generator = Generator()
-        self.critique = Critic()
+        self.critique = PatchCritic()
         self.lr = lr
-        self.n_critic = 5
-        self.lambda_gp = 10.0
+        self.n_critic = 10
 
-        vgg = models.vgg16(pretrained=True)
-        self.feature_extractor = nn.Sequential(*list(vgg.features[:16]))
-        self.feature_extractor.eval()
-        for param in self.feature_extractor.parameters():
-            param.requires_grad = False
-
+        self.feature_extractor = SharedFeaturesExtractor().eval()
+        self.perceptual_loss = PerceptualLoss(self.feature_extractor)
         self.reconstruction_loss = nn.L1Loss()
+        self.texture_loss = TextureLoss(self.feature_extractor)
+        self.diversity_loss = DiversityLoss()
         self.automatic_optimization = False
+
+        self.lambda_gp = lambda_gp
+        self.g_adv_weight = g_adv_weight
+        self.g_rec_weight = g_rec_weight
+        self.g_perc_weight = g_perc_weight
+        self.g_tex_weight = g_tex_weight
+        self.g_div_weight = g_div_weight
 
     def forward(self, damaged_image):
         return self.generator(damaged_image)
-
-    def perceptual_loss(self, img1, img2):
-        features_img1 = self.feature_extractor(img1)
-        features_img2 = self.feature_extractor(img2)
-        return F.l1_loss(features_img1, features_img2)
 
     def training_step(self, batch, batch_idx):
         g_opt, c_opt = self.optimizers()
@@ -99,32 +166,54 @@ class GANInpainting(pl.LightningModule):
         # -----------------------
         # Train Critique
         # -----------------------
-        real_preds = self.critique(original_image)
-        fake_images = self(damaged_image).detach()
-        fake_preds = self.critique(fake_images)
-        c_loss = -(real_preds.mean() - fake_preds.mean())
-        gp = self.gradient_penalty(original_image, fake_images)
-        c_loss += self.lambda_gp * gp
-        self.log("critique_loss", c_loss, on_step=True, on_epoch=True, prog_bar=True)
-        self.manual_backward(c_loss)
-        c_opt.step()
-        c_opt.zero_grad()
+        for _ in range(self.n_critic):
+            real_preds = self.critique(original_image)
+            fake_images = self(damaged_image).detach()
+            fake_preds = self.critique(fake_images)
+            c_loss = torch.mean(fake_preds) - torch.mean(real_preds)
+            gp = self.gradient_penalty(original_image, fake_images)
+            c_loss += self.lambda_gp * gp
+            self.log("critique_loss", c_loss, on_step=True, on_epoch=True, prog_bar=True)
+            self.manual_backward(c_loss)
+            c_opt.step()
+            c_opt.zero_grad()
+            self.clip_critic_weights()
 
         # -----------------------
         # Train Generator
         # -----------------------
         inpainted_image = self(damaged_image)
         fake_preds = self.critique(inpainted_image)
-        g_adv_loss = -fake_preds.mean()
+        g_adv_loss = -torch.mean(fake_preds)
         g_perc_loss = self.perceptual_loss(inpainted_image, original_image)
         g_rec_loss = self.reconstruction_loss(inpainted_image, original_image)
-        g_loss = g_adv_loss + 3 * g_rec_loss + 2 * g_perc_loss
+        g_tex_loss = self.texture_loss(inpainted_image, original_image)
+        g_div_loss = self.diversity_loss(inpainted_image)
+
+        g_loss = (
+            self.g_adv_weight * g_adv_loss +
+            self.g_rec_weight * g_rec_loss +
+            self.g_perc_weight * g_perc_loss +
+            self.g_tex_weight * g_tex_loss +
+            self.g_div_weight * g_div_loss
+        )
+
         self.log("generator_loss", g_loss, on_step=True, on_epoch=True, prog_bar=True)
+        self.log("g_adv_loss", g_adv_loss, on_step=False, on_epoch=True, prog_bar=False)
+        self.log("g_rec_loss", g_rec_loss, on_step=False, on_epoch=True, prog_bar=False)
+        self.log("g_perc_loss", g_perc_loss, on_step=False, on_epoch=True, prog_bar=False)
+        self.log("g_tex_loss", g_tex_loss, on_step=False, on_epoch=True, prog_bar=False)
+        self.log("g_div_loss", g_div_loss, on_step=False, on_epoch=True, prog_bar=False)
+
         self.manual_backward(g_loss)
         g_opt.step()
         g_opt.zero_grad()
 
         return {"generator_loss": g_loss, "critique_loss": c_loss}
+
+    def clip_critic_weights(self):
+        for p in self.critique.parameters():
+            p.data.clamp_(-0.01, 0.01)
 
     def gradient_penalty(self, real_images, fake_images):
         batch_size, c, h, w = real_images.size()
@@ -170,7 +259,7 @@ class GANInpainting(pl.LightningModule):
 
     def configure_optimizers(self):
         g_opt = torch.optim.Adam(self.generator.parameters(), lr=self.lr, betas=(0.5, 0.999))
-        d_opt = torch.optim.Adam(self.critique.parameters(), lr=self.lr, betas=(0.5, 0.999))
+        d_opt = torch.optim.Adam(self.critique.parameters(), lr=self.lr * 0.5, betas=(0.5, 0.999))
         return [g_opt, d_opt]
 
     def _create_image_grid(self, damaged_image, inpainted_image, original_image):

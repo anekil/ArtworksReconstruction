dd1c3dac67aab4a7d9007528fb81f5ec1435b8c0
anekil <235892@edu.p.lodz.pl>
2025-01-24 12:32:00 +0100

    final superresolution model Co-authored-by: Adrian-Rochminski <235959@edu.p.lodz.pl>

--- superresolution/SuperResolutionDataset.py
+++ superresolution/SuperResolutionDataset.py
@@ -1,21 +1,35 @@
+from PIL import Image
 from torchvision import models, transforms
 from dataloading.WikiArtDataset import WikiArtDataset
 
 
 class SuperResolutionDataset(WikiArtDataset):
-    def __init__(self, df, transform=None, upscale_factor=2):
-        super().__init__(df, transform)
-        self.upscale_factor = upscale_factor
+    def __init__(self, image_paths, transform=None, input_size=(224,224), output_size=(448,448)):
+        self.image_paths = image_paths
+        self.input_size = input_size
+        self.transform = transform
+        self.original_transform = transforms.Compose([
+            transforms.CenterCrop(output_size),
+            transforms.ToTensor()
+        ])
+        self.low_res_transform = transforms.Compose([
+            transforms.Resize(input_size, interpolation=transforms.InterpolationMode.BILINEAR),
+            transforms.ToTensor()
+        ])
+
+    def __len__(self):
+        return len(self.image_paths)
 
     def __getitem__(self, idx):
-        image = super().__getitem__(idx)
-        low_res_image = transforms.functional.resize(
-            image, [image.size(1) // self.upscale_factor, image.size(2) // self.upscale_factor],
-            interpolation=transforms.InterpolationMode.BICUBIC
-        )
-        low_res_upsampled = transforms.functional.resize(
-            low_res_image, [image.size(1), image.size(2)],
-            interpolation=transforms.InterpolationMode.BICUBIC
-        )
+        img_path = self.image_paths[idx]
+        image = Image.open(img_path).convert("RGB")
 
-        return low_res_upsampled, image
+        if self.transform:
+            image = self.transform(image)
+
+        original_image = self.original_transform(image)
+        low_res_image = transforms.Resize(self.input_size, interpolation=transforms.InterpolationMode.BICUBIC)(
+            transforms.ToPILImage()(original_image))
+        low_res_image = transforms.ToTensor()(low_res_image)
+
+        return low_res_image, original_image

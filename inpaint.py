5bed8e43171cc82d5662f8db9f0bac985be0410c
anekil <235892@edu.p.lodz.pl>
2025-01-27 01:52:00 +0100

    balanced distribution across clusters in inpainting

--- inpaint.py
+++ inpaint.py
@@ -2,15 +2,15 @@
 import comet_ml
 import torch
 import os
-
 from datasets import load_dataset
 from pytorch_lightning.loggers import CometLogger
 from torchvision.transforms import v2
-
-torch.cuda.empty_cache()
-import pandas as pd
 from inpainting.InpaintingDataset import *
 from torch.utils.data import DataLoader
+from sklearn.model_selection import train_test_split
+import pandas as pd
+
+torch.cuda.empty_cache()
 
 dataset = load_dataset("Artificio/WikiArt_Full", split="train")
 df = dataset.to_pandas()
@@ -18,18 +18,32 @@ cluster_csv_path = "image_label_dict.csv"
 cluster_df = pd.read_csv(cluster_csv_path)
 cluster_df['idx'] = cluster_df['idx'].astype(int)
 merged_df = df.merge(cluster_df, left_index=True, right_on="idx", how="left")
-df = merged_df[['image', 'cluster']].head(10000)
+df = merged_df[['image', 'cluster']]
 
-total_len = len(df)
-train_len = int(0.8 * total_len)
-val_len = int(0.5 * (total_len - train_len))
-train_df = df[:train_len].copy(deep=True)
-val_df = df[train_len:train_len+val_len].copy(deep=True)
-test_df = df[train_len+val_len:].copy(deep=True)
+total_size = len(df)
+new_size = total_size // 2
+df = df.sample(n=new_size, random_state=42)
+grouped = df.groupby('cluster')
+min_samples_per_cluster = grouped.size().min()
+balanced_df = grouped.apply(lambda x: x.sample(n=min_samples_per_cluster, random_state=42)).reset_index(drop=True)
+
+train_val_df, test_df = train_test_split(
+    balanced_df,
+    test_size=0.2,
+    stratify=balanced_df['cluster'],
+    random_state=42
+)
+
+train_df, val_df = train_test_split(
+    train_val_df,
+    test_size=0.25,
+    stratify=train_val_df['cluster'],
+    random_state=42
+)
 
 transform = v2.Compose([
-    v2.RandomCrop((256, 256)),
-    v2.Resize((256, 256)),
+    v2.RandomCrop((224, 224)),
+    v2.Resize((224, 224)),
     v2.RandomHorizontalFlip(),
 ])
 
@@ -39,11 +53,11 @@ to_input = v2.Compose([
 ])
 
 train_dataset = InpaintingDataset(train_df, transform=transform, to_input=to_input)
-train_dataloader = DataLoader(train_dataset, batch_size=16, shuffle=True, num_workers=10)
+train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True, num_workers=10)
 val_dataset = InpaintingDataset(val_df, transform=transform, to_input=to_input)
-val_dataloader = DataLoader(val_dataset, batch_size=16, shuffle=False, num_workers=10)
+val_dataloader = DataLoader(val_dataset, batch_size=32, shuffle=False, num_workers=10)
 test_dataset = InpaintingDataset(test_df, transform=transform, to_input=to_input)
-test_dataloader = DataLoader(test_dataset, batch_size=16, shuffle=False, num_workers=10)
+test_dataloader = DataLoader(test_dataset, batch_size=32, shuffle=False, num_workers=10)
 
 import matplotlib.pyplot as plt
 
